a79de411a77ad116c6b0b8225a6028e952a9ceff
Marcin Moroz <marcinmoroz+subskrypcje@gmail.com>
2020-09-30 09:01:27 +0200

    Initiall commit

---
+++ EncodingConverter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution EncodingConverter");
+            string[] extensions = new[] { ".cs", ".aspx" };
+            var solutionDirectory = @"L:\SourceCodes\Mozart-1.0\code";
+            foreach (var f in new DirectoryInfo(solutionDirectory).GetFiles("*", SearchOption.AllDirectories)
+                .Where(f => extensions.Contains(f.Extension.ToLower())))
+            {
+                string s = readFileAsUtf8(f.FullName);
+                File.WriteAllText(f.FullName, s, new UTF8Encoding(true));
+            }            
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        public static String readFileAsUtf8(string fileName)
+        {
+            Encoding encoding = Encoding.Default;
+            String original = String.Empty;
+
+            using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
+            {
+                original = sr.ReadToEnd();
+                encoding = sr.CurrentEncoding;
+                sr.Close();
+            }
+
+            if (encoding == Encoding.UTF8)
+                return original;
+
+            byte[] encBytes = encoding.GetBytes(original);
+            byte[] utf8Bytes = Encoding.Convert(encoding, Encoding.UTF8, encBytes);
+            return Encoding.UTF8.GetString(utf8Bytes);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 EncodingConverter
+
+I needed to fix encoding for all files in my solution. After going through couple of solution on stackoverflow i came with this
+
+Threads i base it on
+
+https://stackoverflow.com/questions/4386750/convert-ansi-windows-1252-to-utf8-in-c-sharp (djunod)
+https://stackoverflow.com/questions/279673/save-all-files-in-visual-studio-project-as-utf-8 (Timwi)
+
